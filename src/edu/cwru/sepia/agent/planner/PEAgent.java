29e9767344ea451f1023b7c20f89ccff675750e0
Adam J <amj69@case.edu>
2016-03-29 10:43:26 -0400

    gibbed the PEAgent a bit

--- src/edu/cwru/sepia/agent/planner/PEAgent.java
+++ src/edu/cwru/sepia/agent/planner/PEAgent.java
@@ -32,7 +32,7 @@ public class PEAgent extends Agent {
 
     public PEAgent(int playernum, Stack<StripsAction> plan) {
         super(playernum);
-        peasantIdMap = new HashMap<Integer, Integer>();
+        peasantIdMap = new HashMap<>();
         this.plan = plan;
 
     }
@@ -47,76 +47,25 @@ public class PEAgent extends Agent {
                 townhallId = unitId;
             } else if(unitType.equals("peasant")) {
                 peasantIdMap.put(unitId, unitId);
-            }
-        }
-
-        // Gets the peasant template ID. This is used when building a new peasant with the townhall
-        for(Template.TemplateView templateView : stateView.getTemplates(playernum)) {
-            if(templateView.getName().toLowerCase().equals("peasant")) {
-                peasantTemplateId = templateView.getID();
-                break;
+                peasantTemplateId = unit.getTemplateView().getID();
             }
         }
 
         return middleStep(stateView, historyView);
     }
-
-    /**
-     * This is where you will read the provided plan and execute it. If your plan is correct then when the plan is empty
-     * the scenario should end with a victory. If the scenario keeps running after you run out of actions to execute
-     * then either your plan is incorrect or your execution of the plan has a bug.
-     *
-     * You can create a SEPIA deposit action with the following method
-     * Action.createPrimitiveDeposit(int peasantId, Direction townhallDirection)
-     *
-     * You can create a SEPIA harvest action with the following method
-     * Action.createPrimitiveGather(int peasantId, Direction resourceDirection)
-     *
-     * You can create a SEPIA build action with the following method
-     * Action.createPrimitiveProduction(int townhallId, int peasantTemplateId)
-     *
-     * You can create a SEPIA move action with the following method
-     * Action.createCompoundMove(int peasantId, int x, int y)
-     *
-     * these actions are stored in a mapping between the peasant unit ID executing the action and the action you created.
-     *
-     * For the compound actions you will need to check their progress and wait until they are complete before issuing
-     * another action for that unit. If you issue an action before the compound action is complete then the peasant
-     * will stop what it was doing and begin executing the new action.
-     *
-     * To check an action's progress you can call getCurrentDurativeAction on each UnitView. If the Action is null nothing
-     * is being executed. If the action is not null then you should also call getCurrentDurativeProgress. If the value is less than
-     * 1 then the action is still in progress.
-     *
-     * Also remember to check your plan's preconditions before executing!
-     */
+    
     @Override
     public Map<Integer, Action> middleStep(State.StateView stateView, History.HistoryView historyView) {
-        // TODO: Implement me!
-        return null;
-    }
-
-    /**
-     * Returns a SEPIA version of the specified Strips Action.
-     * @param action StripsAction
-     * @return SEPIA representation of same action
-     */
-    private Action createSepiaAction(StripsAction action) {
+        stateView.getAllUnits().get(0).getCurrentDurativeAction();
         return null;
     }
 
     @Override
-    public void terminalStep(State.StateView stateView, History.HistoryView historyView) {
-
-    }
+    public void terminalStep(State.StateView stateView, History.HistoryView historyView) {}
 
     @Override
-    public void savePlayerData(OutputStream outputStream) {
-
-    }
+    public void savePlayerData(OutputStream outputStream) {}
 
     @Override
-    public void loadPlayerData(InputStream inputStream) {
-
-    }
+    public void loadPlayerData(InputStream inputStream) {}
 }
