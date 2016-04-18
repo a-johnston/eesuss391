86a00fbadd6d4111f36118bb6c9464e629c35eb6
Adam J <amj69@case.edu>
2016-04-18 12:09:02 -0400

    added turn penalty probably correctly

--- src/edu/cwru/sepia/agent/RLAgent.java
+++ src/edu/cwru/sepia/agent/RLAgent.java
@@ -142,8 +142,9 @@ public class RLAgent extends Agent {
      */
     @Override
     public Map<Integer, Action> middleStep(StateView stateView, HistoryView historyView) {
+    	Map<Integer, Action> actionMap = new HashMap<>();
     	
-        return null;
+        return actionMap;
     }
 
     /**
@@ -225,7 +226,7 @@ public class RLAgent extends Agent {
     public double calculateReward(StateView stateView, HistoryView historyView, int footmanId) {
     	double newSumReward = 0.0;
     	
-    	// TODO : account for TURN_PENALTY
+    	// TODO : confirm that this is correct
     	
     	for (UnitView unit : getUnitViews(stateView, myFootmen)) {
     		newSumReward += UNIT_BONUS + HP_BONUS * unit.getHP();
@@ -238,7 +239,7 @@ public class RLAgent extends Agent {
     	double delta = newSumReward - sumReward;
     	sumReward = newSumReward;
     	
-        return delta;
+        return TURN_PENALTY + delta;
     }
     
     /**
@@ -250,6 +251,7 @@ public class RLAgent extends Agent {
     private List<UnitView> getUnitViews(StateView stateView, List<Integer> unitIds) {
     	return unitIds.stream()
     			.map(id -> stateView.getUnit(id))
+    			.filter(unit -> unit != null)
     			.collect(Collectors.toList());
     }
 
